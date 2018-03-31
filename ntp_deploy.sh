7b697af7837abb242727a01a0c29f800ed22e1f4
MirgDenis <mirgdenis@gmail.com>
2018-03-31 15:35:20 +0300

    Initial commit

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -2,9 +2,9 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt-get -y install ntp
-sudo sed -i '/[1-9].ubuntu.pool.ntp.org/d' /etc/ntp.conf
-sudo sed -i 's/[0].ubuntu.pool.ntp.org/ua.pool.ntp.org/' /etc/ntp.conf
-cp /etc/ntp.conf /etc/ntp.conf.bak 
+sudo sed -i '/^pool /d' /etc/ntp.conf
+sudo sed -i '/more information/a \pool ua.pool.ntp.org iburst' /etc/ntp.conf
+sudo cp /etc/ntp.conf /etc/ntp.conf.bak 
 sudo service ntp restart
 echo "*/1 * * * * $dir/ntp_verify.sh MAILTO=root@localhost" | crontab -
 
